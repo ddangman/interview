c75ae33a95edbbb51b5de1db197cf624d39dbca0
Duy Dang <duydanglife@gmail.com>
2017-03-24 16:50:41 -0700

    more notes

--- src/com/interview/string/ZAlgorithm.java
+++ src/com/interview/string/ZAlgorithm.java
@@ -19,23 +19,31 @@ public class ZAlgorithm {
 
     private int[] calculateZ(char input[]) {
         int Z[] = new int[input.length];
+        // boundary of Z-box
         int left = 0;
         int right = 0;
-        for(int k = 1; k < input.length; k++) {
+        // nothing to compare 1st char to
+        for(int k = 1; k < input.length; k++) { // k is position on Z[]
             if(k > right) {
-                left = right = k;
+                left = right = k; // reset box to input[0]-input[0]
+                // check if Z-box is within input array
+                // && compare suffix char with prefix char
                 while(right < input.length && input[right] == input[right - left]) {
+                    // char match, extend Z-box boundary rightward
                     right++;
                 }
-                Z[k] = right - left;
+                /* no match */
+                // right to left boundary of Z-box is length of matching substring
+                Z[k] = right - left; // save size and position of matching pattern
                 right--;
             } else {
-                //we are operating inside box
-                int k1 = k - left;
+                //we are operating inside Z-box
+                // Z-box is suffix box that matches input prefix
+                int k1 = k - left; // position in Z-box
                 //if value does not stretches till right bound then just copy it.
                 if(Z[k1] < right - k + 1) {
                     Z[k] = Z[k1];
-                } else { //otherwise try to see if there are more matches.
+                } else { //otherwise try to see if there are more matches beyond Z-box
                     left = k;
                     while(right < input.length && input[right] == input[right - left]) {
                         right++;
@@ -52,12 +60,14 @@ public class ZAlgorithm {
      * Returns list of all indices where pattern is found in text.
      */
     public List<Integer> matchPattern(char text[], char pattern[]) {
+        // holds pattern + separatingChar + text to search
         char newString[] = new char[text.length + pattern.length + 1];
         int i = 0;
         for(char ch : pattern) {
             newString[i] = ch;
             i++;
         }
+        // special char separating pattern from text
         newString[i] = '$';
         i++;
         for(char ch : text) {
@@ -68,7 +78,7 @@ public class ZAlgorithm {
         int Z[] = calculateZ(newString);
 
         for(i = 0; i < Z.length ; i++) {
-            if(Z[i] == pattern.length) {
+            if(Z[i] == pattern.length) { // complete pattern match found
                 result.add(i - pattern.length - 1);
             }
         }
