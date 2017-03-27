537ef38fe6430d941494cea88e7c515d0ac12525
Duy Dang <duydanglife@gmail.com>
2017-03-26 19:34:33 -0700

    prefix only search added

--- src/com/interview/suffixprefix/Trie.java
+++ src/com/interview/suffixprefix/Trie.java
@@ -24,6 +24,7 @@ public class Trie {
     }
 
     private final TrieNode root;
+    // initialize root in constructor
     public Trie() {
         root = new TrieNode();
     }
@@ -35,13 +36,15 @@ public class Trie {
         TrieNode current = root;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            TrieNode node = current.children.get(ch);
-            if (node == null) {
-                node = new TrieNode();
-                current.children.put(ch, node);
+            TrieNode nextNode = current.children.get(ch);
+            if (nextNode == null) { // check if char is already in map
+                // char does not exist, generate and add node to parent
+                nextNode = new TrieNode();
+                current.children.put(ch, nextNode);
             }
-            current = node;
+            current = nextNode;
         }
+        //last character reached
         //mark the current nodes endOfWord as true
         current.endOfWord = true;
     }
@@ -61,32 +64,50 @@ public class Trie {
             return;
         }
         char ch = word.charAt(index);
-        TrieNode node = current.children.get(ch);
+        TrieNode nextNode = current.children.get(ch);
 
         //if node does not exists in map then create one and put it into map
-        if (node == null) {
-            node = new TrieNode();
-            current.children.put(ch, node);
+        if (nextNode == null) {
+            nextNode = new TrieNode();
+            current.children.put(ch, nextNode);
         }
-        insertRecursive(node, word, index + 1);
+        insertRecursive(nextNode, word, index + 1);
     }
 
     /**
      * Iterative implementation of search into trie.
      */
-    public boolean search(String word) {
+    public boolean searchWhole(String word) {
         TrieNode current = root;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            TrieNode node = current.children.get(ch);
+            TrieNode nextNode = current.children.get(ch);
             //if node does not exist for given char then return false
-            if (node == null) {
+            if (nextNode == null) {
                 return false;
             }
-            current = node;
+            current = nextNode;
         }
         //return true of current's endOfWord is true else return false.
+        // if searching whole word match, boolean matters
+        // when only checking prefix, completion of loop should return true
         return current.endOfWord;
+    }
+    
+        public boolean searchPrefix(String word) {
+        TrieNode current = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            TrieNode nextNode = current.children.get(ch);
+            //if node does not exist for given char then return false
+            if (nextNode == null) {
+                return false;
+            }
+            current = nextNode;
+        }
+        // if searching whole word match, boolean matters
+        // when only checking prefix, completion of loop should return true
+        return true;
     }
 
     /**
@@ -101,12 +122,12 @@ public class Trie {
             return current.endOfWord;
         }
         char ch = word.charAt(index);
-        TrieNode node = current.children.get(ch);
+        TrieNode nextNode = current.children.get(ch);
         //if node does not exist for given char then return false
-        if (node == null) {
+        if (nextNode == null) {
             return false;
         }
-        return searchRecursive(node, word, index + 1);
+        return searchRecursive(nextNode, word, index + 1);
     }
 
     /**
@@ -121,26 +142,33 @@ public class Trie {
      */
     private boolean delete(TrieNode current, String word, int index) {
         if (index == word.length()) {
-            //when end of word is reached only delete if currrent.endOfWord is true.
+            //when end of word is reached only delete if current.endOfWord is true.
             if (!current.endOfWord) {
+                // whole word is not matched
                 return false;
             }
+            // TrieNode not empty, do not delete node
+            // change endOfWord to false so future searchWhole will return false
+            // searchPrefix can still return true
             current.endOfWord = false;
             //if current has no other mapping then return true
-            return current.children.size() == 0;
+            // so calling function can delete this reference from their map
+            return current.children.size() == 0; 
         }
         char ch = word.charAt(index);
-        TrieNode node = current.children.get(ch);
-        if (node == null) {
+        TrieNode nextNode = current.children.get(ch);
+        if (nextNode == null) {
+            // character not found, word does not exist
             return false;
         }
-        boolean shouldDeleteCurrentNode = delete(node, word, index + 1);
+        boolean shouldDeleteCurrentNode = delete(nextNode, word, index + 1);
 
         //if true is returned then delete the mapping of character and trienode reference from map.
         if (shouldDeleteCurrentNode) {
             current.children.remove(ch);
             //return true if no mappings are left in the map.
-            return current.children.size() == 0;
+            // if this node has no other character, it will be deleted
+            return current.children.size() == 0; 
         }
         return false;
     }
