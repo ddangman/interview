92f46b0df8d34e775cf990a9fd4c033ffb8ab4e5
Duy Dang <duydanglife@gmail.com>
2017-03-27 22:19:08 -0700

    labeled TreeMap key, value

--- src/com/interview/geometry/SkylineDrawing.java
+++ src/com/interview/geometry/SkylineDrawing.java
@@ -31,10 +31,11 @@ public class SkylineDrawing {
             //If they are same then use this logic
             //if two starts are compared then higher height building should be picked first
             //if two ends are compared then lower height building should be picked first
-            //if one start and end is compared then start should appear before end
+            //if one start and one end is compared then start should be picked before end
             if (this.x != o.x) {
-                return this.x - o.x;
+                return this.x - o.x; // smaller x comes first
             } else {
+                // isStart is given negative value to prioritize
                 return (this.isStart ? -this.height : this.height) - (o.isStart ? -o.height : o.height);
             }
         }
@@ -46,11 +47,13 @@ public class SkylineDrawing {
         BuildingPoint[] buildingPoints = new BuildingPoint[buildings.length*2];
         int index = 0;
         for(int building[] : buildings) {
+            // start point
             buildingPoints[index] = new BuildingPoint();
             buildingPoints[index].x = building[0];
             buildingPoints[index].isStart = true;
             buildingPoints[index].height = building[2];
 
+            // end point
             buildingPoints[index + 1] = new BuildingPoint();
             buildingPoints[index + 1].x = building[1];
             buildingPoints[index + 1].isStart = false;
@@ -59,8 +62,10 @@ public class SkylineDrawing {
         }
         Arrays.sort(buildingPoints);
 
+        //Data structure used must support: add, remove, returnMax
         //using TreeMap because it gives log time performance.
         //PriorityQueue in java does not support remove(object) operation in log time.
+        /* key: height, value: count of height */
         TreeMap<Integer, Integer> queue = new TreeMap<>();
         //PriorityQueue<Integer> queue1 = new PriorityQueue<>(Collections.reverseOrder());
         queue.put(0, 1);
@@ -68,8 +73,8 @@ public class SkylineDrawing {
         int prevMaxHeight = 0;
         List<int[]> result = new ArrayList<>();
         for(BuildingPoint buildingPoint : buildingPoints) {
-            //if it is start of building then add the height to map. If height already exists then increment
-            //the value
+            //if it is start of building then add the height to map. 
+            //If height already exists then increment the value
             if (buildingPoint.isStart) {
                 queue.compute(buildingPoint.height, (key, value) -> {
                     if (value != null) {
@@ -87,14 +92,15 @@ public class SkylineDrawing {
                 });
                // queue1.remove(cp.height);
             }
-            //peek the current height after addition or removal of building x.
-            int currentMaxHeight = queue.lastKey();
+            //peek the current max height after addition or removal of building x.
+            int currentMaxHeight = queue.lastKey(); //return highest key in map
             //int currentMaxHeight = queue1.peek();
             //if height changes from previous height then this building x becomes critcal x.
             // So add it to the result.
-            if (prevMaxHeight != currentMaxHeight) {
+            if (prevMaxHeight != currentMaxHeight) { // max height changed
+                // add current buiding's x, current max height
                 result.add(new int[]{buildingPoint.x, currentMaxHeight});
-                prevMaxHeight = currentMaxHeight;
+                prevMaxHeight = currentMaxHeight; // track max height
             }
         }
         return result;
